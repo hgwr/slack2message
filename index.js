8c8e60ba6eff7ea4b40db87397e4a86eedac457b
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-06-27 17:43:46 +0900

    showing display name

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ app.event('message', async ({ event, context }) => {
   console.log(event)
   // console.log(`${event.message.user} said ${message.text}`);
   if (event.type && event.text && event.type == 'message') {
-    sendMessage(`スラックから転送。${message.user}曰く「${event.text}」`)
+    const result = await app.client.users.profile.get({
+      // The token you used to initialize your app is stored in the `context` object
+      token: context.botToken,
+      user: event.user
+    });
+    sendMessage(`スラックから転送。 ${result.profile.display_name} 曰く、 ${event.text}`)
   }
 })
 
