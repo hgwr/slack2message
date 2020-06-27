5b66c83ce838c9727222fdbb2cae37fe3cf585a8
Shigeru Hagiwara <hgwrsgr@gmail.com>
2020-06-27 16:31:21 +0900

    initial version

---
+++ index.js
@@ -0,0 +1,40 @@
+const { App } = require('@slack/bolt')
+const { exec } = require('child_process')
+
+const sendMessage = (message) => {
+  exec(
+    `imessage --text "${message}" --contacts '${process.env.FORWARDED_FOR}'`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+    }
+  )
+}
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+})
+
+app.event('message', async ({ event, context }) => {
+  console.log(event)
+  // console.log(`${event.message.user} said ${message.text}`);
+  if (event.type && event.text && event.type == 'message') {
+    sendMessage(event.text)
+  }
+})
+
+;(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000)
+
+  console.log('⚡️ Bolt app is running!')
+})()
